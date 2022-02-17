3d5b1a3d387a8f70b8cb09d4c25b1b623f6605b1
samiranGithub <samirankumarroy24@gmail.com>
2022-02-17 21:11:59 +0600

    Final Commit

--- js/app.js
+++ js/app.js
@@ -3,26 +3,21 @@ const errMsg = document.querySelector(".err_msg");
 const pElem = document.createElement("p");
 let throwErr = errMsg.appendChild(pElem);
 
-
-document.getElementById("calc_btn").addEventListener("click", function () {
-   return inputValidation();
-});
-
 function gettingInputValue(id) {
    let inputIdVal = document.getElementById(id + "-input");
-   inputIdVal = incomeInput.value;
+   inputIdVal = inputIdVal.value;
    const inputVal = parseFloat(inputIdVal);
    return inputVal;
 }
 
 function inputValidation() {
    // All Input Values
-   let incomeInput = document.getElementById("income-input");
-   let foodInput = document.getElementById("food-input");
-   let rentInput = document.getElementById("rent-input");
-   let clothesInput = document.getElementById("clothes-input");
+   let incomeInput = document.getElementById("income-input").value;
+   let foodInput = document.getElementById("food-input").value;
+   let rentInput = document.getElementById("rent-input").value;
+   let clothesInput = document.getElementById("clothes-input").value;
 
-   const incomeValue = gettingInputValue("income")
+   const incomeValue = gettingInputValue("income");
    const foodValue = gettingInputValue("food");
    const rentValue = gettingInputValue("rent");
    const clothesValue = gettingInputValue("clothes");
@@ -33,28 +28,28 @@ function inputValidation() {
    } else if (isNaN(incomeValue) && typeof incomeInput != 'number') {
       throwErr.innerText = "Income value should be numbers";
    } else if (incomeValue < 0) {
-      throwErr.innerText = "Negative value are not alllowed";
+      throwErr.innerText = "Negative value are not alllowed in income";
    } else if (foodInput == "") {
       throwErr.innerText = "Please insert your food value";
    } else if (isNaN(foodValue)) {
       throwErr.innerText = "Food value should be number";
    } else if (foodValue < 0) {
-      throwErr.innerText = "Negative value are not alllowed";
+      throwErr.innerText = "Negative value are not alllowed in food";
    } else if (rentInput == "") {
       throwErr.innerText = "Please insert your rent value";
    } else if (isNaN(rentValue)) {
       throwErr.innerText = "Rent value should be number";
    } else if (rentValue < 0) {
-      throwErr.innerText = "Negative value are not alllowed";
+      throwErr.innerText = "Negative value are not alllowed in rent";
    } else if (clothesInput == "") {
       throwErr.innerText = "Please insert your clothes value";
    } else if (isNaN(clothesValue)) {
       throwErr.innerText = "Clothes value should be number";
    } else if (clothesValue < 0) {
-      throwErr.innerText = "Negative value are not alllowed";
+      throwErr.innerText = "Negative value are not alllowed in clothes";
    } else {
       throwErr.innerText = "";
-      calculateExpenses(incomeValue, foodValue, rentValue, clothesValue);
+      return calculateExpenses(incomeValue, foodValue, rentValue, clothesValue);
    }
 }
 
@@ -64,22 +59,56 @@ function calculateExpenses(incomeValue, foodValue, rentValue, clothesValue) {
 
    if (totalExpense > incomeValue) {
       throwErr.innerText = "expense Value greater than income";
+      document.querySelector('.total_expense').innerText = "";
+      document.querySelector('.total_balance').innerText = "";
    } else {
       document.querySelector('.total_expense').innerText = totalExpense;
       document.querySelector('.total_balance').innerText = incomeValue - totalExpense;
+      throwErr.innerText = "";
    }
 }
 
+document.getElementById("calc_btn").addEventListener("click", function () {
+   return inputValidation();
+});
 
+
+// extra saving information
 document.getElementById('saveBtn').addEventListener('click', function () {
-   let saveInput = document.getElementById("saving-input");
-   saveInput = saveInput.value;
-   const saveInputValue = parseFloat(saveInput);
+   // saving input value
+   const savinginput = document.getElementById("saving-input").value;
+
+   // for showing error message in saving section;
+   const showErrInSaving = document.querySelector('.saving_amount_err');
+   let finalAmount = document.querySelector('.final_amount');
+
+   const remainingBalance = document.getElementById("remaining_balance");
 
    let totalBalance = document.querySelector('.total_balance').innerText;
-   const totalBalanceVal = parseFloat(totalBalance);
+   totalBalance = parseFloat(totalBalance);
 
-   let xx = totalBalanceVal / saveInputValue;
-   document.querySelector('.final_amount').innerText = xx;
+   let saveInputValue = gettingInputValue("saving");
+   let incomeAmount = gettingInputValue("income");
+   const totalSave = Math.floor((saveInputValue / 100) * incomeAmount);
 
+   // checking condition
+   if (savinginput == "") {
+      showErrInSaving.innerText = "Please enter value";
+      finalAmount.innerText = "";
+      remainingBalance.innerText = "";
+   } else if (isNaN(saveInputValue) || typeof saveInputValue != "number") {
+      showErrInSaving.innerText = "Only numbers are allowed";
+      finalAmount.innerText = "";
+      remainingBalance.innerText = "";
+   } else {
+      if (totalSave > totalBalance) {
+         showErrInSaving.innerText = "Saving value should not be greater than total balance";
+         finalAmount.innerText = "";
+         remainingBalance.innerText = "";
+      } else {
+         finalAmount.innerText = totalSave;
+         remainingBalance.innerText = totalBalance - totalSave;
+         document.querySelector('.saving_amount_err').innerHTML = "";
+      }
+   }
 });
